675828e2757318254f2b51aff1b7e05adb8df44a
Pratik Majumdar <info.pratikmajumdar@gmail.com>
2024-06-05 02:39:57 +0530

    ADD: UI code for implementation of the terminal user interface

--- internal/ui/ui.go
+++ internal/ui/ui.go
@@ -2,8 +2,36 @@ package ui
 
 import (
 	"fmt"
+
+	"github.com/codadept/schdl/internal/storage"
+	"github.com/manifoldco/promptui"
 )
 
-func Run() {
-	fmt.Println("Hello user, I am schdl")
+// Run the Terminal UI Prompt
+func Run(storage *storage.Storage) {
+	for {
+		prompt := promptui.Select{
+			Label: "Select Action",
+			Items: []string{"Create Task", "List Tasks", "Update Task", "Delete Task", "Exit"},
+		}
+
+		_, action, err := prompt.Run()
+		if err != nil {
+			fmt.Println("Prompt failed:", err)
+			return
+		}
+
+		switch action {
+		case "Create Task":
+			CreateTask(storage)
+		case "List Tasks":
+			ListTasks(storage)
+		case "Update Task":
+			UpdateTask(storage)
+		case "Delete Task":
+			DeleteTask(storage)
+		case "Exit":
+			return
+		}
+	}
 }
